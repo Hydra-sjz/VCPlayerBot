071c0ba8c66812fb0b81475d2b1492bd464c1e84
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-06-15 21:41:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,9 +16,9 @@
 from pyrogram import Client
 from config import Config
 bot = Client(
-    "VCPlayer",
-    Config.API_ID,
-    Config.API_HASH,
+    name="VCPlayer",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
     plugins=dict(root="plugins")
 )
